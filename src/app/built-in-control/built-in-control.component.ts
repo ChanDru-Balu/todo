449ca47f0494ef97bbc392684d90b10789e610d0
ChanDru-Balu <prochandru@gmail.com>
2025-03-02 23:25:44 +0530

    signal , interface , enum

--- src/app/built-in-control/built-in-control.component.ts
+++ src/app/built-in-control/built-in-control.component.ts
@@ -1,6 +1,19 @@
 import { CommonModule, NgIf } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, Signal, signal } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+
+interface Todo {
+  id: number ,
+  task : string ,
+  date: Date,
+  status : string ,
+  isCompleted : Boolean
+}
+
+enum status {
+  PEDNING = 'pending',
+  COMPLETED = 'completed'
+}
 
 @Component({
   selector: 'app-built-in-control',
@@ -12,48 +25,57 @@ import { FormsModule } from '@angular/forms';
   styleUrl: './built-in-control.component.css'
 })
 export class BuiltInControlComponent {
+
   
 
-  todoList: any[] = [];
-  currentTodo: string = '';
-  currentTodoId: number = 0;
-  updateTodoEnable : boolean = false;
+  todoList  = signal<Todo[]>([]);
+  currentTodo = signal<string>('');
+  currentTodoId =  signal<number>(0);
+  updateTodoEnable = signal<boolean>(false);
 
   addTodo(): void {
+    console.log("Add todo called!")
     let todoObj = {
       id: new Date().getTime(),
-      task: this.currentTodo,
+      task: this.currentTodo(),
       date: new Date(),
-      status: "pending",
+      status: status.PEDNING,
       isCompleted: true
     }
-    this.todoList.push(todoObj)
-    this.currentTodo = '';
+    this.todoList.update((todos) => [...todos,todoObj])
+    this.currentTodo.set('')
+    console.log(this.todoList())
+  }
+
+  setCurrentTodo(event: Event) {
+    let currentTodo =  event.target as HTMLInputElement;
+    this.currentTodo.set(currentTodo.value)
   }
 
   updateTodo() {
-    this.todoList.forEach((todo:any)=>{
-      if(todo.id == this.currentTodoId){
-        todo.task = this.currentTodo;
-        this.updateTodoEnable = false ;
-        this.currentTodo = '';
-        this.currentTodoId = 0;
-      }
-    })
+    this.todoList.update((todos)=>
+      todos.map((todo)=>
+        todo.id === this.currentTodoId() ? {...todo,task:this.currentTodo()}: todo
+      )
+    )
+
+    this.updateTodoEnable.set(false) ;
+    this.currentTodo.set('');
+    this.currentTodoId.set(0);
   }
 
   clickTodo(todo:any): void{
-    this.currentTodo = todo.task ;
-    this.currentTodoId = todo.id ;
-    this.updateTodoEnable = true;
+    this.currentTodo.set(todo.task) ;
+    this.currentTodoId.set(todo.id) ;
+    this.updateTodoEnable.set(true);
   }
 
   checkTodo(todo:any): void {
-    if(todo.status == 'pending'){
-      todo.status = 'completed';
-    } else {
-      todo.status = 'pending';
-    }
+    this.todoList.update((todos)=>
+      todos.map((t)=>
+        todo.id === t.id ? {...t , status: t.status === status.PEDNING ? status.COMPLETED : status.PEDNING } : t
+      )
+    )
   }
 
 
